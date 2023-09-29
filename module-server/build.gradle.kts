12ba338c94a2b6a38c3b1557b73a31fb22f8574c
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2023-09-29 22:03:25 +0200

    Add GCloud settings and configuration

--- module-server/build.gradle.kts
+++ module-server/build.gradle.kts
@@ -1,8 +1,20 @@
 plugins {
     id("org.springframework.boot") version "3.1.4"
+    id("com.google.cloud.tools.appengine") version "2.4.4"
+}
+
+appengine {
+    deploy {
+        projectId = "GCLOUD_CONFIG"
+        version = "GCLOUD_CONFIG"
+    }
+    stage {
+        setArtifact("${buildDir}/libs/${project.name}-${project.version}.jar")
+    }
 }
 
 springBoot {
+    // enable versioning and build info to be injected
     buildInfo()
 }
 
@@ -18,6 +30,7 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
 //    implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.cloud:spring-cloud-gcp-starter:1.2.8.RELEASE")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,18 @@ rootProject.name = "recharger"
 
 include("module-api")
 include("module-server")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+//        google()
+        mavenCentral()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.google.cloud.tools.appengine") {
+                useModule("com.google.cloud.tools:appengine-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
